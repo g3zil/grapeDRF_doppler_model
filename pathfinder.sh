0b10c8cbb8f8f0c7173788b89e1c1aebce4a8290
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2025-09-07 11:08:14 +0100

    Update pathfinder.sh

--- pathfinder.sh
+++ pathfinder.sh
@@ -5,11 +5,11 @@
 #
 # This script takes two arguments the name of the *_config.ini file with parameters for PyLap and number of minutes to model
 # PyLap simulations are every 5 minutes, so this best be a multiple of 5 with span within one day, for now...
-# 
+# Do not run over a midnight boundary
 # Version 1.0 Gwyn Griffiths G3ZIL September 2025
 #
 
-# Read the command line variables
+# Read the command line variables config file name and time span in minutes 
 CONFIG_FILE=$1
 CONFIG_PREFIX=$(echo ${CONFIG_FILE} | sed 's/config.ini//' | tr -d "_")
 CONFIG_FILE="./config/"${CONFIG_FILE}
@@ -19,17 +19,17 @@ TIMESPAN=$2
 ITERATIONS=$(echo $((TIMESPAN / 5)))
 echo "Will run ${ITERATIONS} timesteps with config prefix ${CONFIG_PREFIX}"
 
-
+# The time in the config file is as required by PyLap, this code splits out into its components
 YEAR=$(grep 'ut' <${CONFIG_FILE} | sed 's/ut = //' | tr -d "[" | tr -d "]" | awk -F, '{print$1}' | awk '{$1=$1};1')
 MONTH=$(grep 'ut' <${CONFIG_FILE} | sed 's/ut = //' | tr -d "[" | tr -d "]" | awk -F, '{print$2}' | awk '{$1=$1};1')
 DAY=$(grep 'ut' <${CONFIG_FILE} | sed 's/ut = //' | tr -d "[" | tr -d "]" | awk -F, '{print$3}' | awk '{$1=$1};1')
 HOUR=$(grep 'ut' <${CONFIG_FILE} | sed 's/ut = //' | tr -d "[" | tr -d "]" | awk -F, '{print$4}' | awk '{$1=$1};1')
 MINUTE=$(grep 'ut' <${CONFIG_FILE} | sed 's/ut = //' | tr -d "[" | tr -d "]" | awk -F, '{print$5}' | awk '{$1=$1};1')
-
+# keep a copy of the starting hour and minutes
 INIT_HOUR=${HOUR}
 INIT_MINUTE=${MINUTE}
 
-# delete previous instance of the output file from python
+# form a datetime string for use in the csv filename as YYYmmddHHMM, padding mm,dd,HH,MM with 0 if needed
 FILETIME=${YEAR}
 if [ ${#MONTH} = "1" ]
   then FILETIME=${FILETIME}0${MONTH}
@@ -52,10 +52,11 @@ else
   FILETIME=${FILETIME}${MINUTE}
 fi
 
+# delete previous instance of the csv output file with same start time
 if test -f ./output/csv/${CONFIG_PREFIX}/${FILETIME}_pathfinder.csv; then
   rm ./output/csv/${CONFIG_PREFIX}/${FILETIME}_pathfinder.csv
 fi
-
+# Now ready to loop in 5 minute intervals
 for ((i = 0 ; i < ${ITERATIONS} ; i++ ));
 do
   echo "Running python prog at ${HOUR}:${MINUTE}"
@@ -69,7 +70,7 @@ do
   fi
   NEW_UT="ut = ["${YEAR}","${MONTH}","${DAY}","${HOUR}","${MINUTE}"]"
   echo ${NEW_UT}
-  sed -i '/ut =/c\'"${NEW_UT}"'' ${CONFIG_FILE}
+  sed -i '/ut =/c\'"${NEW_UT}"'' ${CONFIG_FILE}     # next time into the config.ini file
 done
 INIT_UT="ut = ["${YEAR}","${MONTH}","${DAY}","${INIT_HOUR}","${INIT_MINUTE}"]"
-sed -i '/ut =/c\'"${INIT_UT}"'' ${CONFIG_FILE}
+sed -i '/ut =/c\'"${INIT_UT}"'' ${CONFIG_FILE}      # reset time in config.ini file to what it was 
