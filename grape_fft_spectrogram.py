d0906d5e893197c0e26356ba2bf2ac3fad99762b
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2025-09-20 13:25:45 +0100

    Update grape_fft_spectrogram.py

--- grape_fft_spectrogram.py
+++ grape_fft_spectrogram.py
@@ -11,18 +11,18 @@
 # Script needs four command line arguments:
 # 1. Directory to process 2. Index of array of frequency to plot
 # 3. Start time in hour   4. Stop time in hour
-#     e.g. python3 grape_digital_RF_metadata.py ch0_G4HZX 6 8 13
-
-# Last modified 23 June 2025 for use with QEX article submission
+# An optional fifth argument 'DB' connects to the database, checks if equivalent data available and if so plots it superimposed
+#     e.g. python3 grape_digital_RF_metadata.py ch0_G4HZX 6 8 13 DB
+# Last modified September 2025 post QEX now with synth spec read/plot from database
 # Gwyn Griffiths G3ZIL with thanks for Nathaniel Frissell W2NAF for metadata data_dict code
 
 import digital_rf as drf
 import numpy as np
 import matplotlib as mpl
 import pylab as plt
-from scipy.fft import fft, fftfreq
+from scipy.fft import fft, fftfreq, fftshift
 from scipy import signal
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytz
 import sys
 import os
@@ -36,12 +36,15 @@ output_dir=os.path.join(base_directory,'output')
 
 do = drf.DigitalRFReader(data_dir)
 
-# check for four command line arguments
+# check for four or five command line arguments
 n = len(sys.argv)
 if n<=4:
    print ("Rerun with channel name, frequency index and start and stop hours as four command line arguments")
    exit()
 if n>5:
+   if sys.argv[5] == 'DB':
+     db_flag='True'
+   else:
    print ("Rerun with channel name, frequency index and start and stop hours as four command line arguments")
    exit()
 
@@ -110,6 +113,7 @@ print ("First data sample is ", data[0])
 
 x=np.linspace(hours_offset,hours_offset+int(length/60), length)
 yf=fftfreq(m_samples,1/fs)
+yf=fftshift(yf)                        # shift the zero frequency to the centre, avoids white line at zero on spectrogram
 
 # generate a Hann window of length m_samples (i.e. 600 samples)
 window = signal.windows.hann(m_samples)
@@ -117,7 +121,7 @@ window = signal.windows.hann(m_samples)
 for j in range (0,length-1):
    k=int(j*m_samples)
    yt=fft(data[k:k+m_samples]*window,norm="forward",overwrite_x=False)*Hann_factor     # do the FFT
-   yt_abs=np.abs(yt)
+   yt_abs=fftshift(np.abs(yt))        # shift zero frequency to centre
    zf=np.column_stack((zf,yt_abs))    # stack the results in the 2D array zf for contour plotting
 
 zf_dB=10*np.log10(zf)	              # Log 10 for Power Spectral Density (PSD)
@@ -136,7 +140,7 @@ xaxis_title="Time on " + date + " (hours UTC)"
 # Use these percentiles to set limits, but add 3 dB to max to be sure
 
 zf_dB_no_nan=zf_dB[~np.isnan(zf_dB)]
-min_level=np.floor(np.percentile(zf_dB_no_nan,5))
+min_level=np.floor(np.percentile(zf_dB_no_nan,10))  # 10% as min, to reduce background shading
 max_level=np.ceil(np.percentile(zf_dB_no_nan,99.9))+3
 levels=np.arange(min_level,max_level+6,3)
 
@@ -148,7 +152,8 @@ cs=ax.contourf(x,yf,zf_dB, levels, cmap="Greys")
 plt.suptitle(plot_title)
 plt.xlabel(xaxis_title)
 plt.ylabel("Doppler shift (Hz)")
-plt.gcf().set_size_inches(8, 3, forward=True)
+plt.gcf().set_size_inches(12, 4.5, forward=True)
+plt.xlim(0,np.ceil(length/60))
 
 cbar = fig.colorbar(cs)
 cbar.set_label("PSD uncalibrated (dB)", rotation=270, labelpad=25)
@@ -156,5 +161,88 @@ plt.tight_layout()
 
 # Save the plot
 plt.savefig(plot_dir + "/Spectrogram" + "_" + str(frequency) + "MHz_" + date + ".png", dpi=600)
+print("PSWS Spectrogram saved")
+
+###################################################################################################
+# Now read the database and superimpose synthetic spectrum if option DB is included in command line
+###################################################################################################
+if db_flag == 'True':
+  print("Database look-up and synthetic spectrum requested")
+  import psycopg2                             # to access the postgresql database
+  import matplotlib.patches as mpatches       # for synth_spec labelling
+
+# initialise database connection flag to None, and status vars to default
+  conn=None
+  connected="Not connected"
+  cursor="No cursor"
+  execute="Not executed"
+
+# get stop and start times in required format from metadata s0 start time and length in command line in minites
+  ts_start = str(datetime.fromtimestamp(s0/10,pytz.utc).strftime('%Y-%m-%d %H:%M:%S'))
+  ts_end = datetime.fromtimestamp(s0/10,pytz.utc) + timedelta(minutes=length)  # length as above in minutes
+  ts_end = str(ts_end.strftime('%Y-%m-%d %H:%M:%S'))
+
+# Construct the SQL statement, with time extracted as hour of the day
+  select_sql="SELECT mod(extract(epoch from time),86400)/3600 as hour,p_mode,color,doppler from synth_spec where time between '"\
+   + ts_start + "' and '" + ts_end + "' and frequency = '" + str(frequency) + "' and rx = '"\
+   + theCallsign + "' ORDER BY time asc "
+  print("SQL statement is: ",select_sql)   # Useful check that it is as required
+
+  data = []
+  try:
+        # connect to the PostgreSQL database on the localhost
+        #print ("Trying to  connect")
+        conn = psycopg2.connect("dbname='hamsci' user='wdread' host='localhost' password='JTWSPR2008'")
+        connected="Connected"
+        #print ("Appear to have connected")
+        cur = conn.cursor()
+        cursor="Got cursor"
+        cur.execute(select_sql)
+        execute="Executed"
+        rows = cur.fetchall()
+        for row in rows:
+          converted_row = []
+          for item in row:
+            try:
+              # try converting to float
+              converted_row.append(float(item))
+            except ValueError:
+              # If error, keep as a string
+              converted_row.append(item)
+          data.append(converted_row)
+        # close communication with the database
+        cur.close()
+        print (connected,cursor, execute)
+  except psycopg2.OperationalError as e:
+        print('Unable to connect!\n{0}').format(e)
+        print ("Unable to connect to the database:", connected,cursor, execute)
+ 
+  np_data=np.array(data)
+  records=len(data)
+  print("Records read from database = ", records)
+
+  if records > 2: 
+    hour=np.float64(np_data[:,0])
+    synth_doppler=np.float64(np_data[:,3])
+  # Plot the synthetic spectrograms as scatterplots, enabling each mode to have its own color as c, s is size
+    ax.scatter=ax.scatter(hour, synth_doppler, c=np_data[:,2], s=2)
+    plt.xlim(0,np.ceil(length/60))
+    #plt.ylim(-1.5,1.5)
+
+    # Create legend manually
+    black_patch = mpatches.Patch(color='firebrick', label='1F')
+    blue_patch = mpatches.Patch(color='blue', label='2F')
+    green_patch = mpatches.Patch(color='green', label='1E')
+    purple_patch = mpatches.Patch(color='purple', label='2E')
+    brown_patch = mpatches.Patch(color='red', label='1Fhi')
+    cyan_patch = mpatches.Patch(color='cyan', label='2Fhi')
+    lime_patch = mpatches.Patch(color='lime', label='1Ehi')
+    orchid_patch = mpatches.Patch(color='orchid', label='2Ehi')
+    plt.legend(handles=[black_patch, blue_patch, green_patch, purple_patch, brown_patch, cyan_patch, lime_patch, orchid_patch],\
+      ncol=2, loc='upper right')
+     
+    plt.savefig(plot_dir + "/Spectrogram+Synth" + "_" + str(frequency) + "MHz_" + date + ".png", dpi=600)
+  else:
+    print("No data in the database to match SQL statement - check it - and or run pathfinder etc.")
 
 plt.show()
