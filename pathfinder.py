cf92e234b21246efd974a91779e404949203cb3e
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2025-12-06 11:29:59 +0000

    Update pathfinder.py

--- pathfinder.py
+++ pathfinder.py
@@ -137,7 +137,7 @@ file_time=sys.argv[2]  # this is date time in form YYYYMMDDHHMM for prefix to cs
 
 ###################################################
 # Consequentials
-elev_inc = 0.002               # set to 0.005 degrees, we are not plotting, and need to get consistently close to same distance tx-rx
+elev_inc = 0.001               # set to 0.005 degrees, we are not plotting, and need to get consistently close to same distance tx-rx
 elevs = np.arange(elev_start, elev_stop, elev_inc, dtype = float) 
 num_elevs = len(elevs)
 proximity=np.empty(num_elevs)
@@ -161,7 +161,7 @@ with open(config_file, 'w') as configfile:
 
 # constants and rarely set options
 speed_of_light = 2.99792458e8
-distance_margin = 20           # try plus minus 2 km, see how many lie in that interval from the true geodesic path distance 
+distance_margin = 50           # try plus minus 2 km, see how many lie in that interval from the true geodesic path distance 
 tol = [1e-7, 0.01, 10]        # py
 doppler_flag = 1              #M generate ionosphere 5 minutes later so that NOT SURE DOPPLER WORKS
                               #M Doppler shift can be calculated
