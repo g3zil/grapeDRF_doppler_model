a2175526d437133fcd32e6f96ab66b8978ea8640
Gwyn Griffiths <gwyn@autonomousanalytics.com>
2026-03-24 08:47:15 +0000

    Refactor x-axis generation in FFT processing
    Updated x-axis generation to use np.ceil for length calculation.

--- grape_fft_spectrogram.py
+++ grape_fft_spectrogram.py
@@ -126,10 +126,7 @@ print ("First data sample is ", data[0])
 ########################################
 # generate the x and y axes for the contour plot 
 
-x=np.linspace(hours_offset,hours_offset+int(length/60), length)
-print(hours_offset,hours_offset+int(length/60), length)
-print (x)
-sys.exit()
+x=np.linspace(hours_offset,hours_offset+np.ceil(length/60), length)
 yf=fftfreq(m_samples,1/fs)
 yf=fftshift(yf)                        # shift the zero frequency to the centre, avoids white line at zero on spectrogram
 
